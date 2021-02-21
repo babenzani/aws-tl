99a11a369e9a43431a547fa96c823335a4682307
babenzani <33708074+babenzani@users.noreply.github.com>
2021-02-21 21:19:52 +0700

    Lifecycle management of AWS resources.

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.29.0"
+    }
+  }
+}
+
 provider "aws" {
   region                  = "ap-southeast-1"
   shared_credentials_file = "/home/arben/.aws/credentials"
