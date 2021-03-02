811a48876792ef6199477c5af89bfaae1971c99b
babenzani <33708074+babenzani@users.noreply.github.com>
2021-03-02 20:27:40 +0700

    modify provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region                  = "ap-southeast-1"
-  shared_credentials_file = "/home/arben/.aws/credentials"
-  profile                 = "arben"
+    region = "${var.AWS_REGION}"
 }
