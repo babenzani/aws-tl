2600a6cab3953020a93a555f917f0027e0b91996
babenzani <33708074+babenzani@users.noreply.github.com>
2021-03-02 20:43:44 +0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,12 @@
 variable "AWS_REGION" {    
     default = "ap-southeast-1"
 }
+
+variable "AMI" {
+    type = "map"
+    
+    default {
+        ap-southeast-1 = "ami-0e2e44c03b85f58b3"
+        us-east-1 = "ami-0c2a1acae6667e438"
+    }
+}
