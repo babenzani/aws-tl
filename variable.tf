746901ea628e091644f03f588fc5d023479255cf
babenzani <33708074+babenzani@users.noreply.github.com>
2021-03-13 06:16:16 +0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,3 +5,15 @@ variable "AWS-REGION" {
 variable "AMI" {
    default  = "ami-0e2e44c03b85f58b3"
 }
+
+variable "PRIVATE_KEY_PATH" {
+  default = "london-region-key-pair"
+}
+
+variable "PUBLIC_KEY_PATH" {
+  default = "london-region-key-pair.pub"
+}
+
+variable "EC2_USER" {
+  default = "ubuntu"
+}
